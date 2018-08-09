6ad07e880cc2b50d5a6ed4fefc329e4ed615739d
Ty Qualters <12106131+SalmonSeasoning@users.noreply.github.com>
2018-08-09 00:26:43 -0500

    basis for reddit memes

--- src/commands/doathing.js
+++ src/commands/doathing.js
@@ -3,10 +3,24 @@
  * @description A module for "doathing" command. (Memes)
  */
 
-const CMD = require('../command.js');
+const CMD = require('../command.js'),
+    https = require('https'),
+    redditURL = "https://www.reddit.com/r/memes/random.json",
+    limit = 1;
+
+/**
+ * @function doRedditCall call reddit with certain parameters
+ */
 
 const command = new CMD.BaseCommand("doathing", CMD.Roles.SingleChannelGuest, (data) => {
-
+    https.get(redditURL + '?limit=' + limit, (response)=>{
+        response.on('data', (d)=>{
+            // TODO
+        });
+        response.on('error', (err)=>{
+            // TODO
+        });
+    });
 });
 
 module.exports = command;
